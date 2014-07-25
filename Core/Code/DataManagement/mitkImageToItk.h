f2274c257e40e5caa778e3f060eb1a2117dc583c
Miklos Espak <m.espak@ucl.ac.uk>
2014-07-25 14:08:02 +0100

    Fix about image read accessor stuff for g++ 4.8 and VS compilers

--- Core/Code/DataManagement/mitkImageToItk.h
+++ Core/Code/DataManagement/mitkImageToItk.h
@@ -28,6 +28,52 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include "mitkImageDataItem.h"
 #include "mitkImageWriteAccessor.h"
 
+/// Helper types for compilers without C++11 support:
+#if __cplusplus <= 199711L
+
+/// Note:
+/// Helper types to check type constness. The ImageToItk class acquires read or
+/// write lock depending whether the output type is const or not.
+
+namespace std
+{
+
+template <typename T, T v>
+struct integral_constant
+{
+  static const T value = v;
+  typedef T value_type;
+  typedef integral_constant<T, v> type;
+};
+
+typedef integral_constant<bool, true> true_type;
+typedef integral_constant<bool, false> false_type;
+
+template<typename T, T v>
+const T std::integral_constant<T, v>::value;
+
+template<typename T> struct is_const : std::false_type {};
+template<typename T> struct is_const<T const> : std::true_type {};
+
+template<typename T> struct remove_const
+{
+  typedef T type;
+};
+
+template<typename T> struct remove_const<T const>
+{
+  typedef T type;
+};
+
+}
+
+#else
+
+#include <type_traits>
+
+#endif
+
+
 namespace mitk
 {
 /**
@@ -38,7 +84,7 @@ namespace mitk
  * \todo Get clear about how to handle directed ITK 2D images in ITK
  */
 template <class TOutputImage>
-class ImageToItk : public itk::ImageSource< TOutputImage >
+class ImageToItk : public itk::ImageSource< typename std::remove_const<TOutputImage>::type >
 {
 protected:
   mitk::Image::Pointer m_MitkImage;
@@ -46,7 +92,7 @@ protected:
 
 public:
   typedef ImageToItk  Self;
-  typedef itk::ImageSource<TOutputImage>  Superclass;
+  typedef itk::ImageSource<typename std::remove_const<TOutputImage>::type>  Superclass;
   typedef itk::SmartPointer<Self>  Pointer;
   typedef itk::SmartPointer<const Self>  ConstPointer;
 
