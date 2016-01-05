faf912bfe9e4564998ebf9fb6854480287e45c0a
Matt Clarkson <m.clarkson@ucl.ac.uk>
2016-01-05 20:42:30 +0000

    Update mitkDataNode.cpp to remove update on non-existant interactor

--- Modules/Core/src/DataManagement/mitkDataNode.cpp
+++ Modules/Core/src/DataManagement/mitkDataNode.cpp
@@ -83,10 +83,6 @@ void mitk::DataNode::SetData(mitk::BaseData* baseData)
 
     m_DataReferenceChangedTime.Modified();
     Modified();
-
-    // inform the interactor about the change
-    if (m_Interactor.IsNotNull())
-      m_Interactor->DataChanged();
   }
 }
 
